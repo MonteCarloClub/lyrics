24188f1eeb10d541f360ccb61d89f0c5a467df87
jindada1 <zou_406143883@qq.com>
2020-12-20 14:42:13 +0800

    收官

--- readme.md
+++ readme.md
@@ -1,4 +1,22 @@
+
 ```shell
 pip install -r requirements.txt
 python ./rapper_lyrics_from_qq.py
 ```
+
+nltk 字典包的安装如果报出`连接远程服务器失败的错误`是因为被墙了，给终端设置代理即可
+
+
+```shell
+
+# cmd 设置代理
+> set HTTP_PROXY=http://127.0.0.1:1080
+> set HTTPS_PROXY=http://127.0.0.1:1080
+
+# 进入 python
+> python
+
+>>> import nltk
+>>> nltk.download()
+```
+弹出一个用`tkinter`实现的GUI界面，点击 `all-corpora`，点击 `Download` 即可。
--- result/analyse.py
+++ result/analyse.py
@@ -1,67 +1,137 @@
 import os
-import enchant
+from nltk.corpus import words
 
-'''
-    rapper                   top200       rest      total
-    -----------------------------------------------------
-    Drake                    201524      91433     292957
-    Eminem                    31294      17130      48424
-    JCole                    104683      52241     156924
-    JuiceWRLD                 41581      19471      61052
-    KendrickLamar            131783      71553     203336
-    LilUziVert                93532      43460     136992
-    LilWayne                 264488     143022     407510
-    PostMalone                41285      16908      58193
-    SnoopDogg                176559      92868     269427
-    TravisScott               66580      34964     101544
-'''
+def count():
+    """
+    统计数量
+    """
 
-root = './useful/sorted'
-output = './analysis'
+    root = './useful/sorted'
+    output = './useful/sorted-filter-strict'
 
-english_words = enchant.Dict("en_US")
+    english_words = words.words()
 
-for rapperfilename in os.listdir(root):
-    
-    totalcount = 0
-    wordtypes = 0   # 词汇量
-    wordcount = 0   # 单词出现的数量
-    digitcount = 0  # 数字出现的数量
-    wrongcount = 0  # 拼写错误的数量
+    print(f'{"rapperfilename":<20} {"wordtypes":>10} {"wordcount":>10} {"digittypes":>10} {"digitcount":>10} {"shittypes":>10} {"shitcount":>10} {"abbrtypes":>10} {"abbrcount":>10} {"conjtypes":>10} {"conjcount":>10} {"wrongtypes":>10} {"wrongcount":>10} {"totalwordcount":>20} {"totalcount":>10} | {"errorlines"}')
 
-    errorlines = 0  # 解析出错的行数
+    for rapperfilename in os.listdir(root):
+        
+        totalcount = 0      # 不管对错，单词总数 
+        totalwordcount = 0  # 符合限制条件的单词数量
 
-    with open(f'{root}/{rapperfilename}', 'r', encoding='gbk') as rapperfile:
+        wordtypes = 0       # 词汇量
+        wordcount = 0       # 单词出现的数量
 
-        line = rapperfile.readline()
+        abbrtypes = 0       # 缩写词的种类
+        abbrcount = 0       # 缩写词出现的数量
 
-        while line.startswith('0'):
+        conjtypes = 0       # 短语种类
+        conjcount = 0       # 短语出现的数量
+
+        digittypes = 0      # 数字种类
+        digitcount = 0      # 数字出现的数量
+
+        shittypes = 0       # 垃圾话单词种类
+        shitcount = 0       # 垃圾话单词的数量
+
+        wrongtypes = 0      # 拼写错误的种类
+        wrongcount = 0      # 拼写错误的数量
+
+        errorlines = 0      # 解析出错的行数
+
+        with open(f'{root}/{rapperfilename}', 'rb') as rapperfile:
+            
+            outputfile = open(f"{output}/{rapperfilename}", mode="w", encoding="utf-8")
+
+            for b_line in rapperfile.readlines():
                 try:
+                    line = b_line.decode('gbk')
+
+                    if line.startswith('0'):
                         emmm, num = line.split('\t')
                         word = emmm[7:]
 
                         # is digit
                         if word.isdigit():
+                            digittypes += 1
                             digitcount += int(num)
                             continue
                         
                         # correct english word
-                        if english_words.check(word):
+                        if word in english_words:
                             wordtypes += 1
                             wordcount += int(num)
+                            outputfile.write(f"{word:<30}{num:>10}")
+
+                        # word contains *
+                        elif '*' in word:
+                            shittypes += 1
+                            shitcount += int(num)
+
+                        # word contains '
+                        elif "'" in word:
+                            abbrtypes += 1
+                            abbrcount += int(num)
+
+                        # word contains -
+                        elif "-" in word:
+                            conjtypes += 1
+                            conjcount += int(num)
                         
                         # wrong english words
                         else:
+                            wrongtypes += 1
                             wrongcount += int(num)
-
-
-        except:
-            errorlines += 1
 
                     else:
                         totalcount = line.split()[-1]
                         totalcount = int(totalcount)
 
+                except:
+                    errorlines += 1
+
+            outputfile.close()
+
+        totalwordcount += wordcount
+
+        print(f'{rapperfilename[:-4]:<20} {wordtypes:>10} {wordcount:>10} {digittypes:>10} {digitcount:>10} {shittypes:>10} {shitcount:>10} {abbrtypes:>10} {abbrcount:>10} {conjtypes:>10} {conjcount:>10} {wrongtypes:>10} {wrongcount:>10} {totalwordcount:>20} {totalcount:>10} | {errorlines}')
 
 
-    print(f'{rapperfilename[:-4]:<20} {wordtypes:>10} {wordcount:>10} {digitcount:>10} {wrongcount:>10} {totalcount:>10}  --- [err] {errorlines}')
+def top10percent():
+    """
+    docstring
+    """
+    inputroot = './useful/sorted-filter-loose'
+    output = './useful/sorted-top10%'
+
+    print(f'{"rapperfilename":<20} {"others":>10} {"top10%":>10}')
+
+    for rapperfilename in os.listdir(inputroot):
+
+        others = 0
+        cores = 0
+
+        with open(f'{inputroot}/{rapperfilename}', 'r', encoding='utf-8') as rapperfile:
+            
+            outputfile = open(f"{output}/{rapperfilename}", mode="w", encoding="utf-8")
+
+            lines = rapperfile.readlines()
+
+            # enheng = int(0.9 * len(lines))
+            enheng = int(len(lines) - 200)
+
+            for linenum, line in enumerate(lines):
+                word, num = line.split()
+
+                if linenum < enheng:
+                    others += int(num)
+
+                else:
+                    cores += int(num)
+                    outputfile.write(f"{word:<30}{num:>10}\n")
+
+            outputfile.close()
+        
+        print(f'{rapperfilename[:-4]:<20} {others:>10} {cores:>10}')
+
+if __name__ == "__main__":
+    top10percent()
